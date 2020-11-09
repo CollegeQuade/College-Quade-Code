062715e9706e9fc588cc71f875d8072190db960d
ChengmingLi-619 <73492582+ChengmingLi-619@users.noreply.github.com>
2020-11-09 13:17:59 -0700

    Add files via upload

---
+++ buzzer.c
@@ -0,0 +1,41 @@
+/*
+ * buzzer.c
+ *
+ *  Created on: 2020Äê11ÔÂ1ÈÕ
+ *      Author: LiChe
+ */
+
+
+#include "buzzer.h"
+
+
+void config_buzzer(void){
+
+
+    TIMER_A0->CTL |= TIMER_A_CTL_CLR;                   // CLear all the setting of TimerA
+    TIMER_A0->CTL |= TIMER_A_CTL_TASSEL_2;              //Choose clock source SMCLK
+    TIMER_A0->CTL |= TIMER_A_CTL_ID__4;                 //ID 4
+    TIMER_A0->CCTL[1] |= TIMER_A_CCTLN_OUTMOD_7;
+    TIMER_A0->CCR[0] = CCR_COMP;
+    TIMER_A0->CCR[1] = CCR_COMP/2;
+}
+
+void config_buzzer_gpio(void){
+    P2->DIR |= BIT4;
+    P2->OUT |= BIT4;
+
+    P2->SEL0 |= BIT4;                               // Let P2.4 output TimerA's outputs  //SEL0=0b01
+    P2->SEL1 &= ~BIT4;                             //SEL1 = 0b00
+
+
+}
+void adjust_buzzer(int x){
+    TIMER_A0->CCR[1] = x*(CCR_COMP/100);
+
+}
+void buzzer_stop(void){
+    TIMER_A0->CTL &= ~(TIMER_A_CTL_MC_0);                  // Stop the TimerA0
+}
+void buzzer_start(void){
+    TIMER_A0->CTL |= TIMER_A_CTL_MC__UP;   }            //Enable the Up mode
+
---
+++ buzzer.h
@@ -0,0 +1,20 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: 2020Äê11ÔÂ1ÈÕ
+ *      Author: LiChe
+ */
+#include "msp.h"
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#define CCR_COMP    ((uint16_t)0x3AA)                  //938 in decimal; (3Mhz/8)*25*10^(-4)s
+//#define CALC_CCR1(X)   CCR_COMP
+
+void config_buzzer(void);
+void adjust_buzzer(int x);
+void config_buzzer_gpio(void);
+void buzzer_start(void);
+void buzzer_stop(void);
+
+#endif /* BUZZER_H_ */
